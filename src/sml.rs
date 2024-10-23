44dc16d105a340d9d1f85822aee58a06f3e9d107
Omar Jatoi <git@omarjatoi.com>
2024-10-22 20:25:39 -0400

    Add default Millet initialization options

--- src/sml.rs
+++ src/sml.rs
@@ -1,10 +1,31 @@
+use zed_extension_api::serde_json;
 use zed_extension_api::{self as zed, Result};
 
-struct SMLExtension;
+struct SMLExtension {}
+
+#[derive(serde::Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct MilletInitOptions {
+    token_hover: bool,
+    fs_watcher: bool,
+    format: String, // "smlfmt", "naive", "none"
+    diagnostics: Diagnostics,
+}
+
+#[derive(serde::Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Diagnostics {
+    on_change: bool,
+    more_info_hint: bool,
+    ignore: String, // "none", "after-syntax", "all"
+}
 
 impl zed::Extension for SMLExtension {
-    fn new() -> Self {
-        Self
+    fn new() -> Self
+    where
+        Self: Sized,
+    {
+        Self {}
     }
 
     fn language_server_command(
@@ -20,6 +41,25 @@ impl zed::Extension for SMLExtension {
             args: vec![],
             env: Default::default(),
         })
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        _worktree: &zed::Worktree,
+    ) -> zed::Result<Option<zed::serde_json::Value>> {
+        let initialization_options = MilletInitOptions {
+            token_hover: true,
+            fs_watcher: true,
+            format: "smlfmt".to_owned(),
+            diagnostics: Diagnostics {
+                on_change: true,
+                more_info_hint: true,
+                ignore: "none".to_owned(),
+            },
+        };
+
+        Ok(Some(serde_json::json!(initialization_options)))
     }
 }
 
