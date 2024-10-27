049fe5e816e29994b6bab177f98082f7d099b538
Omar Jatoi <git@omarjatoi.com>
2024-10-26 20:01:39 -0400

    Add more indendation rules

--- languages/sml/indents.scm
+++ languages/sml/indents.scm
@@ -1,3 +1,25 @@
 (_ "[" "]") @indent
 (_ "{" "}") @indent
 (_ "(" ")") @indent
+
+[
+    (fun_dec)
+    (case_exp)
+    (let_exp)
+    (struct_strexp)
+    (datatype_dec)
+    (conbind)
+    (app_exp)
+    (mrule)
+] @indent
+
+(sig_sigexp
+  "sig" @start
+  "end" @end) @indent
+
+(local_strdec
+  "local" @start
+  "in" @start
+  "end" @end) @indent
+
+"in" @indent
